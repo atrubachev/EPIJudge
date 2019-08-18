551181aa618d96e404486a3d9b038ef8e215f24f
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-08-18 19:08:05 +0200

    Add a solution of PrimeSieve (page 56)

--- epi_judge_java/epi/PrimeSieve.java
+++ epi_judge_java/epi/PrimeSieve.java
@@ -3,14 +3,32 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PrimeSieve {
     @EpiTest(testDataFile = "prime_sieve.tsv")
     // Given n, return all primes up to and including n.
     public static List<Integer> generatePrimes(int n) {
-        // TODO - you fill in here.
-        return null;
+        if (n < 2) return Collections.emptyList();
+
+        List<Integer> primes = new ArrayList<>();
+        primes.add(2);
+        for (int i = 3; i <= n; i++) {
+            if ((i % 2 != 0) && isPrime(primes, i)) primes.add(i);
+        }
+
+        return primes;
+    }
+
+    private static boolean isPrime(List<Integer> primes, int n) {
+        for (int i = 0; i < primes.size() && i < Math.sqrt(n); i++) {
+            if (n % primes.get(i) == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
