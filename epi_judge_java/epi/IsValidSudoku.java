5d0238dbff32b2ab32ea1cc391cc984c5cd469a2
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-08-28 20:35:13 +0200

    Add a solution of IsValidSudoku (page 67)

--- epi_judge_java/epi/IsValidSudoku.java
+++ epi_judge_java/epi/IsValidSudoku.java
@@ -3,6 +3,7 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class IsValidSudoku {
@@ -10,8 +11,34 @@ public class IsValidSudoku {
 
     // Check if a partially filled matrix has any conflicts.
     public static boolean isValidSudoku(List<List<Integer>> partialAssignment) {
-        // TODO - you fill in here.
+        // check rows
+        for (var r = 0; r < partialAssignment.size(); r++) {
+            if (hasDuplicate(partialAssignment, r, 0, r + 1, partialAssignment.size())) return false;
+        }
+        // check cols
+        for (var c = 0; c < partialAssignment.size(); c++) {
+            if (hasDuplicate(partialAssignment, 0, c, partialAssignment.size(), c + 1)) return false;
+        }
+
+        for (var r = 0; r < partialAssignment.size() - 2; r += 3) {
+            for (var c = 0; c < partialAssignment.size() - 2; c += 3) {
+                if (hasDuplicate(partialAssignment, r, c, r + 3, c + 3)) return false;
+            }
+        }
+
         return true;
+    }
+
+    private static boolean hasDuplicate(List<List<Integer>> A, int rowStart, int colStart, int rowStop, int colStop) {
+        var seen = new HashSet<Integer>();
+        for (var r = rowStart; r < rowStop; r++) {
+            for (var c = colStart; c < colStop; c++) {
+                var v = A.get(r).get(c);
+                if (seen.contains(v)) return true;
+                if (v != 0) seen.add(v);
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
