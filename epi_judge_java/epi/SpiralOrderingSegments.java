2e665b869c891eefcf20bd927e4167e3103a2d0e
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-08-28 21:11:57 +0200

    Add a solution of SpiralOrderingSegments (page 69)

--- epi_judge_java/epi/SpiralOrderingSegments.java
+++ epi_judge_java/epi/SpiralOrderingSegments.java
@@ -3,15 +3,45 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SpiralOrderingSegments {
     @EpiTest(testDataFile = "spiral_ordering_segments.tsv")
 
-    public static List<Integer>
-    matrixInSpiralOrder(List<List<Integer>> squareMatrix) {
-        // TODO - you fill in here.
-        return null;
+    public static List<Integer> matrixInSpiralOrder(List<List<Integer>> squareMatrix) {
+        var n = squareMatrix.size();
+        var spiralOrder = new ArrayList<Integer>(n * n);
+        var moves = new Move[]{new Move(0, 1), new Move(1, 0), new Move(0, -1), new Move(-1, 0)};
+        var dir = 0;
+        var r = 0;
+        var c = 0;
+
+        for (var i = 0; i < n * n; i++) {
+            spiralOrder.add(squareMatrix.get(r).get(c));
+            squareMatrix.get(r).set(c, 0);
+            var nR = r + moves[dir].r;
+            var nC = c + moves[dir].c;
+            if (nR < 0 || nR >= n || nC < 0 || nC >= n || squareMatrix.get(nR).get(nC) == 0) {
+                dir = (dir + 1) % 4;
+                nR = r + moves[dir].r;
+                nC = c + moves[dir].c;
+            }
+            r = nR;
+            c = nC;
+        }
+
+        return spiralOrder;
+    }
+
+    static class Move {
+        int r;
+        int c;
+
+        Move(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
     }
 
     public static void main(String[] args) {
