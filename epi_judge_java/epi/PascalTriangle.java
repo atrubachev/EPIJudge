c27404fb05cbd0556cde93db33f47a7172dc8eb0
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-09-03 20:13:58 +0200

    Add a solution of PascalTriangle (page 74)

--- epi_judge_java/epi/PascalTriangle.java
+++ epi_judge_java/epi/PascalTriangle.java
@@ -3,14 +3,27 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PascalTriangle {
     @EpiTest(testDataFile = "pascal_triangle.tsv")
 
     public static List<List<Integer>> generatePascalTriangle(int numRows) {
-        // TODO - you fill in here.
-        return null;
+        List<List<Integer>> matrix = new ArrayList<>(numRows);
+
+        for (int row = 0; row < numRows; row++) {
+            matrix.add(new ArrayList<>(numRows));
+            for (int col = 0; col < row + 1; col++) {
+                if (col == 0 || row == col) {
+                    matrix.get(row).add(1);
+                } else {
+                    matrix.get(row).add(matrix.get(row - 1).get(col - 1) + matrix.get(row - 1).get(col));
+                }
+            }
+        }
+
+        return matrix;
     }
 
     public static void main(String[] args) {
