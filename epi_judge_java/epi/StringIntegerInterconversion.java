6b3775628c862a5b1f4f1308719156a0427023eb
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-09-05 21:38:17 +0200

    Add a solution of StringIntegerInterconversion (page 77)

--- epi_judge_java/epi/StringIntegerInterconversion.java
+++ epi_judge_java/epi/StringIntegerInterconversion.java
@@ -4,16 +4,65 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 
-public class StringIntegerInterconversion {
+import java.util.HashMap;
+import java.util.Map;
 
+public class StringIntegerInterconversion {
     public static String intToString(int x) {
-        // TODO - you fill in here.
-        return "";
+        if (x == 0) {
+            return "0";
+        }
+
+        boolean negative = false;
+        if (x < 0) {
+            negative = true;
+        }
+
+        StringBuilder sBuilder = new StringBuilder();
+
+        int carry = x;
+        while (carry != 0) {
+            sBuilder.append(Math.abs(carry % 10));
+            carry /= 10;
+        }
+
+        if (negative) {
+            sBuilder.append("-");
+        }
+
+        return sBuilder.reverse().toString();
     }
 
     public static int stringToInt(String s) {
-        // TODO - you fill in here.
-        return 0;
+        boolean negative = false;
+        if (s.charAt(0) == '-') {
+            negative = true;
+        }
+
+        Map<Character, Integer> digits = new HashMap<>();
+        digits.put('0', 0);
+        digits.put('1', 1);
+        digits.put('2', 2);
+        digits.put('3', 3);
+        digits.put('4', 4);
+        digits.put('5', 5);
+        digits.put('6', 6);
+        digits.put('7', 7);
+        digits.put('8', 8);
+        digits.put('9', 9);
+
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (digits.containsKey(s.charAt(i))) {
+                result *= 10;
+                result += digits.get(s.charAt(i));
+            }
+        }
+
+        if (negative) {
+            result *= -1;
+        }
+        return result;
     }
 
     @EpiTest(testDataFile = "string_integer_interconversion.tsv")
