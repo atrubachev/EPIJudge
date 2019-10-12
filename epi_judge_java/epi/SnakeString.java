2b1266c1eba94d5e8b71950b139d1cdd2e1f98bf
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-10-12 21:55:54 +0200

    Add the solution of Write a String Sunisoidally problem (page 107 (java book))

--- epi_judge_java/epi/SnakeString.java
+++ epi_judge_java/epi/SnakeString.java
@@ -7,8 +7,19 @@ public class SnakeString {
     @EpiTest(testDataFile = "snake_string.tsv")
 
     public static String snakeString(String s) {
-        // TODO - you fill in here.
-        return "";
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1; i < s.length(); i += 4) {
+            result.append(s.charAt(i));
+        }
+        for (int i = 0; i < s.length(); i += 2) {
+            result.append(s.charAt(i));
+        }
+        for (int i = 3; i < s.length(); i += 4) {
+            result.append(s.charAt(i));
+        }
+
+        return result.toString();
     }
 
     public static void main(String[] args) {
