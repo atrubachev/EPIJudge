37f926ecf440e368b0ec4d48cd9c3601fce37d4e
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-10-14 21:54:06 +0200

    Add the solution of Merge Two Sorted Lists problem (page 115 (java book))

--- epi_judge_java/epi/SortedListsMerge.java
+++ epi_judge_java/epi/SortedListsMerge.java
@@ -8,8 +8,26 @@ public class SortedListsMerge {
     //@include
     public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
                                                         ListNode<Integer> L2) {
-        // TODO - you fill in here.
-        return null;
+        ListNode<Integer> root = new ListNode<>(-1, null);
+        ListNode<Integer> node = root;
+
+        while (L1 != null && L2 != null) {
+            if (L1.data < L2.data) {
+                node.next = L1;
+                L1 = L1.next;
+            } else {
+                node.next = L2;
+                L2 = L2.next;
+            }
+            node = node.next;
+        }
+        if (L1 != null) {
+            node.next = L1;
+        } else {
+            node.next = L2;
+        }
+
+        return root.next;
     }
 
     public static void main(String[] args) {
