9c54999aec3306a78a0882ac8ca009b2214e70b1
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-10-15 21:06:54 +0200

    Add the solution of Reverse a Single Sublist problem (page 116 (java book))

--- epi_judge_java/epi/ReverseSublist.java
+++ epi_judge_java/epi/ReverseSublist.java
@@ -8,8 +8,43 @@ public class ReverseSublist {
 
     public static ListNode<Integer> reverseSublist(ListNode<Integer> L, int start,
                                                    int finish) {
-        // TODO - you fill in here.
-        return null;
+
+        if (L == null || start == finish) {
+            return L;
+        }
+
+        ListNode<Integer> newHead = new ListNode<>(-1, L);
+        ListNode<Integer> sublistHead = newHead;
+
+        int k = 1;
+        while (k++ < start) {
+            sublistHead = sublistHead.next;
+        }
+
+        ListNode<Integer> node = sublistHead.next;
+        while (start++ < finish) {
+            ListNode<Integer> temp = node.next;
+            node.next = temp.next;
+            temp.next = sublistHead.next;
+            sublistHead.next = temp;
+        }
+
+        return newHead.next;
+    }
+
+    private static ListNode<Integer> reverse(ListNode<Integer> L) {
+        ListNode<Integer> node = L;
+        ListNode<Integer> next = null;
+        ListNode<Integer> prev = null;
+
+        while (node != null) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+
+        return prev;
     }
 
     public static void main(String[] args) {
