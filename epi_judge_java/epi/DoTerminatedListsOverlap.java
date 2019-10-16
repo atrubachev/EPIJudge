c4bb8885fc1117d37845e9d64df41faece90fcbc
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-10-16 21:29:52 +0200

    Add the solution of Test For Overlapping Lists problem (page 119 (java book))

--- epi_judge_java/epi/DoTerminatedListsOverlap.java
+++ epi_judge_java/epi/DoTerminatedListsOverlap.java
@@ -8,9 +8,40 @@ import epi.test_framework.TimedExecutor;
 public class DoTerminatedListsOverlap {
 
     public static ListNode<Integer>
-    overlappingNoCycleLists(ListNode<Integer> l0, ListNode<Integer> l1) {
-        // TODO - you fill in here.
-        return null;
+    overlappingNoCycleLists(ListNode<Integer> l1, ListNode<Integer> l2) {
+
+        int k1 = length(l1);
+        int k2 = length(l2);
+
+        if (k1 > k2) {
+            l1 = skipFirstN(l1, k1 - k2);
+        } else {
+            l2 = skipFirstN(l2, k2 - k1);
+        }
+
+        while (l1 != null && l2 != null && l1 != l2) {
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        return l1;
+    }
+
+    private static int length(ListNode<Integer> l) {
+        int k = 0;
+        while (l != null) {
+            k++;
+            l = l.next;
+        }
+        return k;
+    }
+
+    private static ListNode<Integer> skipFirstN(ListNode<Integer> l, int n) {
+        while (l != null && n > 0) {
+            n--;
+            l = l.next;
+        }
+        return l;
     }
 
     @EpiTest(testDataFile = "do_terminated_lists_overlap.tsv")
