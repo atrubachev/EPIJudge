9cc634c83a74cbfaa5240c1abd0de127498bc148
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-10-28 13:49:33 +0100

    Add the solution of Remove Duplicates from a Sorted List problem (page 124 (java book))

--- epi_judge_java/epi/RemoveDuplicatesFromSortedList.java
+++ epi_judge_java/epi/RemoveDuplicatesFromSortedList.java
@@ -7,8 +7,17 @@ public class RemoveDuplicatesFromSortedList {
     @EpiTest(testDataFile = "remove_duplicates_from_sorted_list.tsv")
 
     public static ListNode<Integer> removeDuplicates(ListNode<Integer> L) {
-        // TODO - you fill in here.
-        return null;
+        ListNode<Integer> node = L;
+        while (node != null) {
+            ListNode<Integer> next = node.next;
+            while (next != null && node.data == next.data) {
+                next = next.next;
+                node.next = next;
+            }
+            node = node.next;
+        }
+
+        return L;
     }
 
     public static void main(String[] args) {
