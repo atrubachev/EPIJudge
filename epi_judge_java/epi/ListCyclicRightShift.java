7edc14144850339fe3f3b38314afc285aaca0eb8
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-10-28 16:41:16 +0100

    Add the solution of Implement Cyclic Right Shift problem (page 125 (java book))

--- epi_judge_java/epi/ListCyclicRightShift.java
+++ epi_judge_java/epi/ListCyclicRightShift.java
@@ -6,10 +6,50 @@ import epi.test_framework.GenericTest;
 public class ListCyclicRightShift {
     @EpiTest(testDataFile = "list_cyclic_right_shift.tsv")
 
-    public static ListNode<Integer> cyclicallyRightShiftList(ListNode<Integer> L,
-                                                             int k) {
-        // TODO - you fill in here.
-        return null;
+    public static ListNode<Integer> cyclicallyRightShiftList(ListNode<Integer> L, int k) {
+        if (L == null) {
+            return L;
+        }
+
+        int length = length(L);
+        int shift = k % length;
+        ListNode<Integer> oldHead = L;
+        ListNode<Integer> newHead = cutListOnKthNode(L, length - shift);
+        return joinLists(newHead, oldHead);
+    }
+
+    private static ListNode<Integer> joinLists(ListNode<Integer> L1, ListNode<Integer> L2) {
+        if (L1 == null) {
+            return L2;
+        }
+
+        ListNode<Integer> node = L1;
+        while (node.next != null) {
+            node = node.next;
+        }
+        node.next = L2;
+
+        return L1;
+    }
+
+    private static ListNode<Integer> cutListOnKthNode(ListNode<Integer> L, int k) {
+        ListNode<Integer> tail = L;
+        for (int i = 1; i < k; i++) {
+            tail = tail.next;
+        }
+        ListNode<Integer> head = tail.next;
+        tail.next = null;
+        return head;
+    }
+
+    private static int length(ListNode<Integer> L) {
+        int length = 0;
+        ListNode<Integer> node = L;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+        return length;
     }
 
     public static void main(String[] args) {
