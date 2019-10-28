4523e13866af95960bcf9fa15ba485d28f93f7e0
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-10-28 20:01:26 +0100

    Add the solution of Implement Even-Odd merge problem (page 126 (java book))

--- epi_judge_java/epi/EvenOddListMerge.java
+++ epi_judge_java/epi/EvenOddListMerge.java
@@ -3,14 +3,35 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.List;
-
 public class EvenOddListMerge {
     @EpiTest(testDataFile = "even_odd_list_merge.tsv")
 
     public static ListNode<Integer> evenOddMerge(ListNode<Integer> L) {
-        // TODO - you fill in here.
-        return null;
+        if (L == null) {
+            return L;
+        }
+
+        ListNode<Integer> even = new ListNode<>(-1, null);
+        ListNode<Integer> newHead = even;
+        ListNode<Integer> odd = new ListNode<>(-1, null);
+        ListNode<Integer> oddHead = odd;
+        int count = 0;
+        while (L != null) {
+            if (count % 2 == 0) {
+                even.next = L;
+                even = even.next;
+            } else {
+                odd.next = L;
+                odd = odd.next;
+            }
+            L = L.next;
+            count++;
+        }
+
+        even.next = oddHead.next;
+        odd.next = null;
+
+        return newHead.next;
     }
 
     public static void main(String[] args) {
