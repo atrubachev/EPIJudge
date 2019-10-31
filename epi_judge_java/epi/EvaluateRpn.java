339998073f911dd71a6093d0479ed4f740ac4fc4
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-10-31 22:08:28 +0100

    Add the solution of Evaluate RPN Expressions problem (page 135 (java book))

--- epi_judge_java/epi/EvaluateRpn.java
+++ epi_judge_java/epi/EvaluateRpn.java
@@ -3,12 +3,38 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Set;
+
 public class EvaluateRpn {
     @EpiTest(testDataFile = "evaluate_rpn.tsv")
 
     public static int eval(String expression) {
-        // TODO - you fill in here.
-        return 0;
+        Deque<Integer> terms = new LinkedList<>();
+        Set<String> ops = Set.of("/", "*", "+", "-");
+        for (String element : expression.split(",")) {
+            if (ops.contains(element)) {
+                terms.push(evalOp(terms.pop(), terms.pop(), element));
+            } else {
+                terms.push(Integer.parseInt(element));
+            }
+        }
+
+        return terms.pop();
+    }
+
+    private static Integer evalOp(int a, int b, String op) {
+        switch (op) {
+            case "/":
+                return b / a;
+            case "*":
+                return b * a;
+            case "+":
+                return b + a;
+            default:
+                return b - a;
+        }
     }
 
     public static void main(String[] args) {
