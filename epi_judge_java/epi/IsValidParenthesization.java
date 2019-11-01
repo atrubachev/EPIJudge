9233726baf0d89f4e974f66b8e4ae127f2e72223
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-01 07:18:51 +0100

    Add the solution of Is Valid Parenthesization problem (page 137 (java book))

--- epi_judge_java/epi/IsValidParenthesization.java
+++ epi_judge_java/epi/IsValidParenthesization.java
@@ -3,12 +3,25 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Map;
+
 public class IsValidParenthesization {
     @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
 
     public static boolean isWellFormed(String s) {
-        // TODO - you fill in here.
-        return true;
+        Deque<Character> parenthesises = new LinkedList<>();
+        Map<Character, Character> pairs = Map.of('}', '{', ')', '(', ']', '[');
+        for (Character parenthesis : s.toCharArray()) {
+            if (parenthesises.isEmpty() || !parenthesises.peek().equals(pairs.get(parenthesis))) {
+                parenthesises.push(parenthesis);
+            } else {
+                parenthesises.pop();
+            }
+        }
+
+        return parenthesises.isEmpty();
     }
 
     public static void main(String[] args) {
