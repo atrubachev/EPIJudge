b354887c76d349af1b5b34b63d47cc18586af943
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-03 08:54:56 +0100

    Add the solution of Compute Binary Tree Nodes In Order Of Increasing Depth problem (page 143 (java book))

--- epi_judge_java/epi/TreeLevelOrder.java
+++ epi_judge_java/epi/TreeLevelOrder.java
@@ -3,6 +3,9 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TreeLevelOrder {
@@ -10,8 +13,37 @@ public class TreeLevelOrder {
 
     public static List<List<Integer>>
     binaryTreeDepthOrder(BinaryTreeNode<Integer> tree) {
-        // TODO - you fill in here.
-        return null;
+        Deque<NodeWithDepth> nodes = new LinkedList<>();
+        if (tree != null) {
+            nodes.add(new NodeWithDepth(tree, 0));
+        }
+        List<List<Integer>> result = new ArrayList<>();
+
+        while (!nodes.isEmpty()) {
+            NodeWithDepth node = nodes.poll();
+            if (result.size() - 1 < node.depth) {
+                result.add(new ArrayList<>());
+            }
+            result.get(node.depth).add(node.node.data);
+            if (node.node.left != null) {
+                nodes.add(new NodeWithDepth(node.node.left, node.depth + 1));
+            }
+            if (node.node.right != null) {
+                nodes.add(new NodeWithDepth(node.node.right, node.depth + 1));
+            }
+        }
+
+        return result;
+    }
+
+    public static class NodeWithDepth {
+        BinaryTreeNode<Integer> node;
+        Integer depth;
+
+        public NodeWithDepth(BinaryTreeNode node, Integer depth) {
+            this.node = node;
+            this.depth = depth;
+        }
     }
 
     public static void main(String[] args) {
