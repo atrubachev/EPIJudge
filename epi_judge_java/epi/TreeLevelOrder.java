ecbb5d38232e3fb6737487a585592a8606726c53
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-03 09:00:57 +0100

    Improve the solution of Compute Binary Tree Nodes In Order Of Increasing Depth problem (page 143 (java book))

--- epi_judge_java/epi/TreeLevelOrder.java
+++ epi_judge_java/epi/TreeLevelOrder.java
@@ -11,39 +11,31 @@ import java.util.List;
 public class TreeLevelOrder {
     @EpiTest(testDataFile = "tree_level_order.tsv")
 
-    public static List<List<Integer>>
-    binaryTreeDepthOrder(BinaryTreeNode<Integer> tree) {
-        Deque<NodeWithDepth> nodes = new LinkedList<>();
+    public static List<List<Integer>> binaryTreeDepthOrder(BinaryTreeNode<Integer> tree) {
+        Deque<BinaryTreeNode<Integer>> nodes = new LinkedList<>();
         if (tree != null) {
-            nodes.add(new NodeWithDepth(tree, 0));
+            nodes.add(tree);
         }
         List<List<Integer>> result = new ArrayList<>();
 
         while (!nodes.isEmpty()) {
-            NodeWithDepth node = nodes.poll();
-            if (result.size() - 1 < node.depth) {
-                result.add(new ArrayList<>());
+            Deque<BinaryTreeNode<Integer>> nextNodes = new LinkedList<>();
+            List<Integer> values = new ArrayList<>();
+            result.add(values);
+            while (!nodes.isEmpty()) {
+                BinaryTreeNode<Integer> node = nodes.poll();
+                values.add(node.data);
+                if (node.left != null) {
+                    nextNodes.add(node.left);
+                }
+                if (node.right != null) {
+                    nextNodes.add(node.right);
+                }
             }
-            result.get(node.depth).add(node.node.data);
-            if (node.node.left != null) {
-                nodes.add(new NodeWithDepth(node.node.left, node.depth + 1));
-            }
-            if (node.node.right != null) {
-                nodes.add(new NodeWithDepth(node.node.right, node.depth + 1));
-            }
+            nodes = nextNodes;
         }
 
         return result;
-    }
-
-    public static class NodeWithDepth {
-        BinaryTreeNode<Integer> node;
-        Integer depth;
-
-        public NodeWithDepth(BinaryTreeNode node, Integer depth) {
-            this.node = node;
-            this.depth = depth;
-        }
     }
 
     public static void main(String[] args) {
