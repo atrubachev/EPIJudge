e7cde1473d70834c7f0df15ba213960266d8e053
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-06 21:40:30 +0100

    Improve the solution of Test if a Binary Tree is Symmetric problem (page 154 (java book))

--- epi_judge_java/epi/IsTreeSymmetric.java
+++ epi_judge_java/epi/IsTreeSymmetric.java
@@ -3,12 +3,38 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class IsTreeSymmetric {
     @EpiTest(testDataFile = "is_tree_symmetric.tsv")
 
     public static boolean isSymmetric(BinaryTreeNode<Integer> tree) {
-        // TODO - you fill in here.
-        return true;
+        if (tree == null) {
+            return true;
+        }
+
+        Deque<BinaryTreeNode<Integer>> left = new LinkedList<>();
+        Deque<BinaryTreeNode<Integer>> right = new LinkedList<>();
+        left.addLast(tree.left);
+        right.addLast(tree.right);
+
+        while (!left.isEmpty() && !right.isEmpty()) {
+            BinaryTreeNode<Integer> leftNode = left.removeFirst();
+            BinaryTreeNode<Integer> rightNode = right.removeFirst();
+            if (leftNode == null && rightNode == null) {
+                continue;
+            }
+            if (leftNode == null || rightNode == null || !leftNode.data.equals(rightNode.data)) {
+                return false;
+            }
+            left.addLast(leftNode.left);
+            right.addLast(rightNode.right);
+            left.addLast(leftNode.right);
+            right.addLast(rightNode.left);
+        }
+
+        return left.isEmpty() && right.isEmpty();
     }
 
     public static void main(String[] args) {
