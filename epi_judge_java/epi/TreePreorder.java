76755f644bbc346f4fdba2e0da79c1922700b477
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-10 07:55:44 +0100

    Add the solution of Implement an Preorder Traversal without Recursion problem (page 161 (java book))

--- epi_judge_java/epi/TreePreorder.java
+++ epi_judge_java/epi/TreePreorder.java
@@ -3,14 +3,32 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TreePreorder {
     @EpiTest(testDataFile = "tree_preorder.tsv")
 
     public static List<Integer> preorderTraversal(BinaryTreeNode<Integer> tree) {
-        // TODO - you fill in here.
-        return null;
+        Deque<BinaryTreeNode<Integer>> nodes = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        BinaryTreeNode<Integer> node = tree;
+        if (node != null) {
+            nodes.addFirst(node);
+        }
+        while (!nodes.isEmpty()) {
+            node = nodes.removeFirst();
+            result.add(node.data);
+            if (node.right != null) {
+                nodes.addFirst(node.right);
+            }
+            if (node.left != null) {
+                nodes.addFirst(node.left);
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
