9ca891bc7d87520af10ae753ec8bd90eb2c26f74
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-13 20:44:58 +0100

    Add the solution of Implement an InOrder Traversal with O(1) Space problem (page 164 (java book))

--- epi_judge_java/epi/TreeWithParentInorder.java
+++ epi_judge_java/epi/TreeWithParentInorder.java
@@ -3,14 +3,35 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class TreeWithParentInorder {
     @EpiTest(testDataFile = "tree_with_parent_inorder.tsv")
 
     public static List<Integer> inorderTraversal(BinaryTree<Integer> tree) {
-        // TODO - you fill in here.
-        return null;
+        List<Integer> result = new ArrayList<>();
+        BinaryTree<Integer> prev = null, curr = tree;
+        while (Objects.nonNull(curr)) {
+            BinaryTree<Integer> next;
+            if (curr.parent == prev) {
+                if (Objects.nonNull(curr.left)) {
+                    next = curr.left;
+                } else {
+                    result.add(curr.data);
+                    next = Objects.nonNull(curr.right) ? curr.right : curr.parent;
+                }
+            } else if (curr.left == prev) {
+                result.add(curr.data);
+                next = Objects.nonNull(curr.right) ? curr.right : curr.parent;
+            } else {
+                next = curr.parent;
+            }
+            prev = curr;
+            curr = next;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
