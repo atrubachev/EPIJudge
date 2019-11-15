c2bfea2fa07132ecadffa64910ebd7babe3c60c7
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-15 07:01:42 +0100

    Add the solution of Reconstruct a Binary Tree from a Preorder Traversal with Markers Space problem (page 167 (java book))

--- epi_judge_java/epi/TreeFromPreorderWithNull.java
+++ epi_judge_java/epi/TreeFromPreorderWithNull.java
@@ -6,17 +6,31 @@ import epi.test_framework.TimedExecutor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class TreeFromPreorderWithNull {
-    public static BinaryTreeNode<Integer>
-    reconstructPreorder(List<Integer> preorder) {
-        // TODO - you fill in here.
-        return null;
+    private static Integer subtreeIdx;
+
+    private static BinaryTreeNode<Integer> reconstructPreorder(List<Integer> preorder) {
+        subtreeIdx = 0;
+        return reconstructHelper(preorder);
+    }
+
+    private static BinaryTreeNode<Integer> reconstructHelper(List<Integer> preorder) {
+        Integer data = preorder.get(subtreeIdx++);
+        if (Objects.isNull(data)) {
+            return null;
+        }
+
+        BinaryTreeNode<Integer> node = new BinaryTreeNode<>(data);
+        node.left = reconstructHelper(preorder);
+        node.right = reconstructHelper(preorder);
+
+        return node;
     }
 
     @EpiTest(testDataFile = "tree_from_preorder_with_null.tsv")
-    public static BinaryTreeNode<Integer>
-    reconstructPreorderWrapper(TimedExecutor executor, List<String> strings)
+    public static BinaryTreeNode<Integer> reconstructPreorderWrapper(TimedExecutor executor, List<String> strings)
             throws Exception {
         List<Integer> ints = new ArrayList<>();
         for (String s : strings) {
