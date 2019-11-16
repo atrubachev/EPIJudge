46dda690339c4f6c7e6683bba622100a76dab076
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-16 10:07:01 +0100

    Add the solution of Compute the Exterior of a Binary Tree problem (page 169 (java book))

--- epi_judge_java/epi/TreeExterior.java
+++ epi_judge_java/epi/TreeExterior.java
@@ -5,16 +5,62 @@ import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class TreeExterior {
 
-    public static List<BinaryTreeNode<Integer>>
+    private static List<BinaryTreeNode<Integer>>
     exteriorBinaryTree(BinaryTreeNode<Integer> tree) {
-        // TODO - you fill in here.
-        return Collections.emptyList();
+        List<BinaryTreeNode<Integer>> exterior = new ArrayList<>();
+        if (tree == null) {
+            return exterior;
+        }
+        exterior.add(tree);
+        if (!isLeaf(tree)) {
+            exterior.addAll(computeEdge(tree.left, false));
+            exterior.addAll(computeLeaves(tree));
+            exterior.addAll(computeEdge(tree.right, true));
+        }
+        return exterior;
+    }
+
+    private static List<BinaryTreeNode<Integer>> computeEdge(BinaryTreeNode<Integer> tree, boolean isMirrored) {
+        List<BinaryTreeNode<Integer>> exterior = new ArrayList<>();
+        BinaryTreeNode<Integer> node = tree;
+        while (node != null && !isLeaf(node)) {
+            exterior.add(node);
+            if (isMirrored) {
+                node = node.right != null ? node.right : node.left;
+            } else {
+                node = node.left != null ? node.left : node.right;
+            }
+        }
+        if (isMirrored) {
+            Collections.reverse(exterior);
+        }
+        return exterior;
+    }
+
+    private static List<BinaryTreeNode<Integer>> computeLeaves(BinaryTreeNode<Integer> tree) {
+        Deque<BinaryTreeNode<Integer>> nodes = new LinkedList<>();
+        List<BinaryTreeNode<Integer>> result = new ArrayList<>();
+        nodes.addFirst(tree);
+        while (!nodes.isEmpty()) {
+            BinaryTreeNode<Integer> node = nodes.removeFirst();
+            if (node == null) {
+                continue;
+            }
+            if (isLeaf(node)) {
+                result.add(node);
+            }
+            nodes.addFirst(node.right);
+            nodes.addFirst(node.left);
+        }
+        return result;
+    }
+
+    private static boolean isLeaf(BinaryTreeNode<Integer> node) {
+        return node.left == null && node.right == null;
     }
 
     private static List<Integer> createOutputList(List<BinaryTreeNode<Integer>> L)
