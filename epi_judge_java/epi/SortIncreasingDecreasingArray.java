68da03be2df3a08c2984200bbb2bcd4e7e94485a
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-18 21:00:37 +0100

    Add the solution of Sort an Increasing-Decreasing Array problem (page 179 (java book))

--- epi_judge_java/epi/SortIncreasingDecreasingArray.java
+++ epi_judge_java/epi/SortIncreasingDecreasingArray.java
@@ -3,14 +3,31 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SortIncreasingDecreasingArray {
     @EpiTest(testDataFile = "sort_increasing_decreasing_array.tsv")
 
     public static List<Integer> sortKIncreasingDecreasingArray(List<Integer> A) {
-        // TODO - you fill in here.
-        return null;
+        List<List<Integer>> lists = new ArrayList<>();
+        int startIdx = 0;
+        boolean isIncreasing = true;
+        for (int i = 1; i <= A.size(); i++) {
+            if (i == A.size() || (A.get(i - 1) > A.get(i) && isIncreasing) || (A.get(i - 1) < A.get(i) && !isIncreasing)) {
+                lists.add(A.subList(startIdx, i));
+                startIdx = i;
+                if (isIncreasing) {
+                    isIncreasing = false;
+                } else {
+                    isIncreasing = true;
+                    Collections.reverse(lists.get(lists.size() - 1));
+                }
+            }
+        }
+
+        return SortedArraysMerge.mergeSortedArrays(lists);
     }
 
     public static void main(String[] args) {
--- epi_judge_java/epi/SortedArraysMerge.java
+++ epi_judge_java/epi/SortedArraysMerge.java
@@ -3,15 +3,50 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class SortedArraysMerge {
-    @EpiTest(testDataFile = "sorted_arrays_merge.tsv")
+    private static class ArrayEntry {
+        int currIdx;
+        List<Integer> array;
 
-    public static List<Integer>
-    mergeSortedArrays(List<List<Integer>> sortedArrays) {
-        // TODO - you fill in here.
-        return null;
+        ArrayEntry(int currIdx, List<Integer> array) {
+            this.currIdx = currIdx;
+            this.array = array;
+        }
+    }
+
+    @EpiTest(testDataFile = "sorted_arrays_merge.tsv")
+    public static List<Integer> mergeSortedArrays(List<List<Integer>> sortedArrays) {
+        PriorityQueue<ArrayEntry> minHeap = new PriorityQueue<>(sortedArrays.size(),
+                Comparator.comparingInt(o -> o.array.get(o.currIdx)));
+        for (List<Integer> array : sortedArrays) {
+            minHeap.add(new ArrayEntry(0, array));
+        }
+        List<Integer> result = new ArrayList<>();
+        while (!minHeap.isEmpty()) {
+            ArrayEntry currMin = minHeap.poll();
+            while (minHeap.peek() != null &&
+                    currMin.array.get(currMin.currIdx) <= minHeap.peek().array.get(minHeap.peek().currIdx)) {
+                result.add(currMin.array.get(currMin.currIdx));
+                currMin.currIdx++;
+                if (currMin.currIdx == currMin.array.size()) {
+                    break;
+                }
+            }
+            if (minHeap.peek() == null) {
+                for (int i = currMin.currIdx; i < currMin.array.size(); i++) {
+                    result.add(currMin.array.get(i));
+                }
+            } else if (currMin.currIdx < currMin.array.size()) {
+                minHeap.add(new ArrayEntry(currMin.currIdx, currMin.array));
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
