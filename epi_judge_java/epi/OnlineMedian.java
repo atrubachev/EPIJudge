515eb4583151dbc8d9ea1ae87a1aa19ea8779968
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-19 21:32:11 +0100

    Add the solution of Compute the Median of Online Data problem (page 182 (java book))

--- epi_judge_java/epi/OnlineMedian.java
+++ epi_judge_java/epi/OnlineMedian.java
@@ -3,13 +3,41 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class OnlineMedian {
-    public static List<Double> onlineMedian(Iterator<Integer> sequence) {
-        // TODO - you fill in here.
-        return null;
+    private static final int INITIAL_SIZE = 16;
+
+    private static List<Double> onlineMedian(Iterator<Integer> sequence) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(INITIAL_SIZE);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(INITIAL_SIZE, Collections.reverseOrder());
+        List<Double> result = new ArrayList<>();
+        while (sequence.hasNext()) {
+            Integer n = sequence.next();
+            if (minHeap.isEmpty()) {
+                minHeap.add(n);
+            } else {
+                if (n >= minHeap.peek()) {
+                    minHeap.add(n);
+                } else {
+                    maxHeap.add(n);
+                }
+            }
+
+            if (minHeap.size() < maxHeap.size()) {
+                minHeap.add(maxHeap.remove());
+            } else if (minHeap.size() - 1 > maxHeap.size()) {
+                maxHeap.add(minHeap.remove());
+            }
+
+            if (minHeap.size() == maxHeap.size()) {
+                result.add((minHeap.peek() + maxHeap.peek()) / 2.0);
+            } else {
+                result.add(Double.valueOf(minHeap.peek()));
+            }
+        }
+
+        return result;
     }
 
     @EpiTest(testDataFile = "online_median.tsv")
