ad3ed0c59ba7b0017995bb60806dd76177d92e11
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-19 20:11:31 +0100

    Add the solution of Sort an Almost Sorted Array problem (page 180 (java book))

--- epi_judge_java/epi/SortAlmostSortedArray.java
+++ epi_judge_java/epi/SortAlmostSortedArray.java
@@ -3,20 +3,31 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class SortAlmostSortedArray {
 
-    public static List<Integer>
-    sortApproximatelySortedData(Iterator<Integer> sequence, int k) {
-        // TODO - you fill in here.
-        return null;
+    private static List<Integer> sortApproximatelySortedData(Iterator<Integer> sequence, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k + 1);
+        List<Integer> result = new ArrayList<>();
+        while (sequence.hasNext()) {
+            if (minHeap.size() == k + 1) {
+                result.add(minHeap.remove());
+            }
+            minHeap.add(sequence.next());
+        }
+        while (!minHeap.isEmpty()) {
+            result.add(minHeap.remove());
+        }
+
+        return result;
     }
 
     @EpiTest(testDataFile = "sort_almost_sorted_array.tsv")
-    public static List<Integer>
-    sortApproximatelySortedDataWrapper(List<Integer> sequence, int k) {
+    public static List<Integer> sortApproximatelySortedDataWrapper(List<Integer> sequence, int k) {
         return sortApproximatelySortedData(sequence.iterator(), k);
     }
 
