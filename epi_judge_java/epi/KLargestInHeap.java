1d0a47349ffbcb31a019cee834bdf57968cb5b97
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-20 22:05:32 +0100

    Add the solution of Compute the K Largest Elements in a Max-Heap problem (page 184 (java book))

--- epi_judge_java/epi/KLargestInHeap.java
+++ epi_judge_java/epi/KLargestInHeap.java
@@ -7,14 +7,41 @@ import epi.test_framework.GenericTest;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.function.BiPredicate;
 
 public class KLargestInHeap {
-    @EpiTest(testDataFile = "k_largest_in_heap.tsv")
 
+    static class Node {
+        int idx, value;
+
+        public Node(int idx, int value) {
+            this.idx = idx;
+            this.value = value;
+        }
+    }
+
+    @EpiTest(testDataFile = "k_largest_in_heap.tsv")
     public static List<Integer> kLargestInBinaryHeap(List<Integer> A, int k) {
-        // TODO - you fill in here.
-        return null;
+        List<Integer> result = new ArrayList<>(k);
+        PriorityQueue<Node> minHeap = new PriorityQueue<>((a, b) -> b.value - a.value);
+        if (!A.isEmpty()) {
+            minHeap.add(new Node(0, A.get(0)));
+        }
+        while (!minHeap.isEmpty() && result.size() != k) {
+            Node node = minHeap.remove();
+            result.add(node.value);
+            int leftIdx = node.idx * 2 + 1;
+            int rightIdx = node.idx * 2 + 2;
+            if (leftIdx < A.size()) {
+                minHeap.add(new Node(leftIdx, A.get(leftIdx)));
+            }
+            if (rightIdx < A.size()) {
+                minHeap.add(new Node(rightIdx, A.get(rightIdx)));
+            }
+        }
+
+        return result;
     }
 
     @EpiTestComparator
