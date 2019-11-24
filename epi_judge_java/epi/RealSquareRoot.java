483cfcbd5594d51a21277a89fd68ecdc07320be0
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-24 09:53:53 +0100

    Add the solution of Compute The Real Square Root problem (page 195 (java book))

--- epi_judge_java/epi/RealSquareRoot.java
+++ epi_judge_java/epi/RealSquareRoot.java
@@ -7,8 +7,35 @@ public class RealSquareRoot {
     @EpiTest(testDataFile = "real_square_root.tsv")
 
     public static double squareRoot(double x) {
-        // TODO - you fill in here.
-        return 0.0;
+        double l = 1.0;
+        double r = x;
+        if (x < 1.0) {
+            l = x;
+            r = 1.0;
+        }
+        while (compare(l, r) < 0) {
+            double m = l + (r - l) / 2;
+            double mSquared = m * m;
+            if (compare(mSquared, x) == 0) {
+                return m;
+            } else if (compare(mSquared, x) > 0) {
+                r = m;
+            } else {
+                l = m;
+            }
+        }
+        return l;
+    }
+
+    private static int compare(double a, double b) {
+        final double epsilon = 0.000001;
+        double diff = (a - b) / b;
+        if (diff < -epsilon) {
+            return -1;
+        } else if (diff > epsilon) {
+            return 1;
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
