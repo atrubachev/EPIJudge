49a239c7294e20727678c1232b87b6cccaadbe8b
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-25 21:55:56 +0100

    Add the solution of Test For Palindromic Permutation problem (page 212 (java book))

--- epi_judge_java/epi/IsStringPermutableToPalindrome.java
+++ epi_judge_java/epi/IsStringPermutableToPalindrome.java
@@ -3,12 +3,36 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsStringPermutableToPalindrome {
     @EpiTest(testDataFile = "is_string_permutable_to_palindrome.tsv")
 
     public static boolean canFormPalindrome(String s) {
-        // TODO - you fill in here.
+        Map<Character, Integer> freq = computeCharacterFres(s);
+        boolean existsOdd = false;
+        for (Map.Entry<Character, Integer> chFreq : freq.entrySet()) {
+            if (chFreq.getValue() % 2 != 0) {
+                if (existsOdd) {
+                    return false;
+                }
+                existsOdd = true;
+            }
+        }
         return true;
+    }
+
+    private static Map<Character, Integer> computeCharacterFres(String s) {
+        Map<Character, Integer> freq = new HashMap<>();
+        for (Character ch : s.toCharArray()) {
+            if (freq.containsKey(ch)) {
+                freq.put(ch, freq.get(ch) + 1);
+            } else {
+                freq.put(ch, 1);
+            }
+        }
+        return freq;
     }
 
     public static void main(String[] args) {
