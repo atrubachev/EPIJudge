5daf91ead667c0e87f0f7af68deb023708943931
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-11-27 06:41:50 +0100

    Add the solution of Find The Nearest Repeated Entries In An Array problem (page 217 (java book))

--- epi_judge_java/epi/NearestRepeatedEntries.java
+++ epi_judge_java/epi/NearestRepeatedEntries.java
@@ -3,14 +3,28 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class NearestRepeatedEntries {
     @EpiTest(testDataFile = "nearest_repeated_entries.tsv")
 
     public static int findNearestRepetition(List<String> paragraph) {
-        // TODO - you fill in here.
-        return 0;
+        Map<String, Integer> wordToIdx = new HashMap<>();
+        int distance = Integer.MAX_VALUE;
+        for (int i = 0; i < paragraph.size(); i++) {
+            String word = paragraph.get(i);
+            if (wordToIdx.containsKey(word)) {
+                distance = Math.min(distance, i - wordToIdx.get(word));
+            }
+            wordToIdx.put(word, i);
+        }
+
+        if (distance == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return distance;
     }
 
     public static void main(String[] args) {
