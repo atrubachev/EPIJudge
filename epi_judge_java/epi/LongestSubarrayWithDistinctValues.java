467666ae4ef863a8acf86e9a3e2c21601f2481c0
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-12-01 16:57:19 +0100

    Add the solution of Find The Smallest Subarray With Distinct Entries problem (page 224 (java book))

--- epi_judge_java/epi/LongestSubarrayWithDistinctValues.java
+++ epi_judge_java/epi/LongestSubarrayWithDistinctValues.java
@@ -3,14 +3,29 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LongestSubarrayWithDistinctValues {
     @EpiTest(testDataFile = "longest_subarray_with_distinct_values.tsv")
 
     public static int longestSubarrayWithDistinctEntries(List<Integer> A) {
-        // TODO - you fill in here.
-        return 0;
+        Set<Integer> seen = new HashSet<>();
+        int count = 0;
+        int max = 0;
+
+        for (int i = 0; i < A.size(); i++) {
+            while (!seen.isEmpty() && seen.contains(A.get(i))) {
+                seen.remove(A.get(i - count));
+                count--;
+            }
+            seen.add(A.get(i));
+            count++;
+            max = Math.max(max, count);
+        }
+
+        return max;
     }
 
     public static void main(String[] args) {
