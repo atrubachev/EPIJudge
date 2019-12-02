25815cb68fd00c2896dd23a7a43744d0bca73a91
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-12-02 07:03:28 +0100

    Add the solution of Find The Length Of A Longest Contained Interval problem (page 225 (java book))

--- epi_judge_java/epi/LongestContainedInterval.java
+++ epi_judge_java/epi/LongestContainedInterval.java
@@ -3,14 +3,37 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LongestContainedInterval {
     @EpiTest(testDataFile = "longest_contained_interval.tsv")
 
     public static int longestContainedRange(List<Integer> A) {
-        // TODO - you fill in here.
-        return 0;
+        Set<Integer> nums = new HashSet<>(A);
+        int maxInterval = 0;
+
+        while (!nums.isEmpty()) {
+            int n = nums.iterator().next();
+            nums.remove(n);
+
+            int leftN = n - 1;
+            while (nums.contains(leftN)) {
+                nums.remove(leftN);
+                leftN--;
+            }
+
+            int rightN = n + 1;
+            while (nums.contains(rightN)) {
+                nums.remove(rightN);
+                rightN++;
+            }
+
+            maxInterval = Math.max(maxInterval, rightN - leftN - 1);
+        }
+
+        return maxInterval;
     }
 
     public static void main(String[] args) {
