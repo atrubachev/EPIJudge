37f9725d740b8d732138d143c651cdfba008bfde
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-12-04 21:43:03 +0100

    Add the solution of Merge Two Sorted Arrays problem (page 238 (java book))

--- epi_judge_java/epi/TwoSortedArraysMerge.java
+++ epi_judge_java/epi/TwoSortedArraysMerge.java
@@ -7,10 +7,20 @@ import java.util.List;
 
 public class TwoSortedArraysMerge {
 
-    public static void mergeTwoSortedArrays(List<Integer> A, int m,
-                                            List<Integer> B, int n) {
-        // TODO - you fill in here.
-        return;
+    private static void mergeTwoSortedArrays(List<Integer> A, int m, List<Integer> B, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (A.get(i) > B.get(j)) {
+                A.set(k--, A.get(i--));
+            } else {
+                A.set(k--, B.get(j--));
+            }
+        }
+        while (j >= 0) {
+            A.set(k--, B.get(j--));
+        }
     }
 
     @EpiTest(testDataFile = "two_sorted_arrays_merge.tsv")
