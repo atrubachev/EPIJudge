f10380d315267f85ee43156a18d386d5a82692d4
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-12-08 12:33:52 +0100

    Add the solution of Compute A Salary Threshold problem (page 252 (java book))

--- epi_judge_java/epi/FindSalaryThreshold.java
+++ epi_judge_java/epi/FindSalaryThreshold.java
@@ -3,15 +3,23 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
 
 public class FindSalaryThreshold {
     @EpiTest(testDataFile = "find_salary_threshold.tsv")
 
-    public static double findSalaryCap(int targetPayroll,
-                                       List<Integer> currentSalaries) {
-        // TODO - you fill in here.
-        return 0.0;
+    public static double findSalaryCap(int targetPayroll, List<Integer> currentSalaries) {
+        Collections.sort(currentSalaries);
+        double salarySum = 0;
+        for (int i = 0; i < currentSalaries.size(); i++) {
+            double sumOfSalaryCap = currentSalaries.get(i) * (currentSalaries.size() - i);
+            if (salarySum + sumOfSalaryCap >= targetPayroll) {
+                return (targetPayroll - salarySum) / (currentSalaries.size() - i);
+            }
+            salarySum += currentSalaries.get(i);
+        }
+        return -1.0;
     }
 
     public static void main(String[] args) {
