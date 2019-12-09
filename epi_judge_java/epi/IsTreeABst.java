e1748acf4a2ae8aff33084218d14150de02576c9
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-12-09 22:43:06 +0100

    Improve the solution of Test If A Binary Tree Satisfy The BST Property problem (page 256 (java book))

--- epi_judge_java/epi/IsTreeABst.java
+++ epi_judge_java/epi/IsTreeABst.java
@@ -3,11 +3,37 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-public class IsTreeABst {
-    @EpiTest(testDataFile = "is_tree_a_bst.tsv")
+import java.util.Deque;
+import java.util.LinkedList;
 
-    public static boolean isBinaryTreeBST(BinaryTreeNode<Integer> tree) {
-        // TODO - you fill in here.
+public class IsTreeABst {
+
+    private static class NodeWithLimits {
+        BinaryTreeNode<Integer> node;
+        Integer smaller, bigger;
+
+        public NodeWithLimits(BinaryTreeNode<Integer> node, Integer smaller, Integer bigger) {
+            this.node = node;
+            this.smaller = smaller;
+            this.bigger = bigger;
+        }
+    }
+
+    @EpiTest(testDataFile = "is_tree_a_bst.tsv")
+    public static boolean isBinaryTreeBST(BinaryTreeNode<Integer> root) {
+        Deque<NodeWithLimits> queue = new LinkedList<>();
+        queue.addFirst(new NodeWithLimits(root, Integer.MIN_VALUE, Integer.MAX_VALUE));
+        while (!queue.isEmpty()) {
+            NodeWithLimits node = queue.removeLast();
+            if (node.node == null) {
+                continue;
+            }
+            if ((node.smaller.compareTo(node.node.data) > 0) || (node.bigger.compareTo(node.node.data) < 0)) {
+                return false;
+            }
+            queue.addFirst(new NodeWithLimits(node.node.left, node.smaller, node.node.data));
+            queue.addFirst(new NodeWithLimits(node.node.right, node.node.data, node.bigger));
+        }
         return true;
     }
 
