5c75f88e5b8467dbf2d4226ce786575db31ce185
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-12-10 20:26:40 +0100

    Find the solution of Find The K Largest Elements A BST problem (page 260 (java book))

--- epi_judge_java/epi/KLargestValuesInBst.java
+++ epi_judge_java/epi/KLargestValuesInBst.java
@@ -4,16 +4,28 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.function.BiPredicate;
 
 public class KLargestValuesInBst {
     @EpiTest(testDataFile = "k_largest_values_in_bst.tsv")
 
     public static List<Integer> findKLargestInBst(BstNode<Integer> tree, int k) {
-        // TODO - you fill in here.
-        return null;
+        List<Integer> result = new ArrayList<>();
+        Deque<BstNode<Integer>> stack = new LinkedList<>();
+        BstNode<Integer> curr = tree;
+        int count = 0;
+        while ((curr != null || !stack.isEmpty()) && count < k) {
+            while (curr != null) {
+                stack.addFirst(curr);
+                curr = curr.right;
+            }
+            curr = stack.removeFirst();
+            count++;
+            result.add(curr.data);
+            curr = curr.left;
+        }
+        return result;
     }
 
     @EpiTestComparator
