b203adeea4a34e2cd5d444ed838883b0bf323b24
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-12-12 08:00:59 +0100

    Add the solution of Reconstruct A BST From Traversal Data problem (page 262 (java book))

--- epi_judge_java/epi/BstFromPreorder.java
+++ epi_judge_java/epi/BstFromPreorder.java
@@ -8,10 +8,20 @@ import java.util.List;
 public class BstFromPreorder {
     @EpiTest(testDataFile = "bst_from_preorder.tsv")
 
-    public static BstNode<Integer>
-    rebuildBSTFromPreorder(List<Integer> preorderSequence) {
-        // TODO - you fill in here.
-        return null;
+    public static BstNode<Integer> rebuildBSTFromPreorder(List<Integer> preorderSequence) {
+        return rebuildBst(preorderSequence, 0, preorderSequence.size());
+    }
+
+    private static BstNode<Integer> rebuildBst(List<Integer> sequence, int start, int end) {
+        if (start >= end) {
+            return null;
+        }
+        Integer root = sequence.get(start);
+        int nextRootStart = start + 1;
+        while (nextRootStart < end && root.compareTo(sequence.get(nextRootStart)) > 0) {
+            nextRootStart++;
+        }
+        return new BstNode<>(root, rebuildBst(sequence, start + 1, nextRootStart), rebuildBst(sequence, nextRootStart, end));
     }
 
     public static void main(String[] args) {
