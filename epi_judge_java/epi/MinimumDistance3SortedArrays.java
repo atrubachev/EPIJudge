b0d2fe4eabf91b143825b9eddd09afebe91a980e
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-12-14 10:31:52 +0100

    Add the solution of Find The Closest Entries In Three Sorted Arrays problem (page 265 (java book))

--- epi_judge_java/epi/MinimumDistance3SortedArrays.java
+++ epi_judge_java/epi/MinimumDistance3SortedArrays.java
@@ -3,7 +3,10 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
 public class MinimumDistance3SortedArrays {
 
@@ -27,11 +30,32 @@ public class MinimumDistance3SortedArrays {
     }
 
     @EpiTest(testDataFile = "minimum_distance_3_sorted_arrays.tsv")
+    public static int findMinDistanceSortedArrays(List<List<Integer>> sortedArrays) {
+        List<Integer> headsOfArrays = new ArrayList<>(sortedArrays.size());
+        for (int i = 0; i < sortedArrays.size(); i++) {
+            headsOfArrays.add(0);
+        }
 
-    public static int
-    findMinDistanceSortedArrays(List<List<Integer>> sortedArrays) {
-        // TODO - you fill in here.
-        return 0;
+        int minDistance = Integer.MAX_VALUE;
+        NavigableSet<ArrayData> minElements = new TreeSet<>();
+
+        for (int i = 0; i < sortedArrays.size(); i++) {
+            List<Integer> sortedArray = sortedArrays.get(i);
+            minElements.add(new ArrayData(i, sortedArray.get(0)));
+        }
+
+        while (true) {
+            minDistance = Math.min(minDistance, minElements.last().val - minElements.first().val);
+            ArrayData currMin = minElements.first();
+            int nextMinIdx = headsOfArrays.get(currMin.idx) + 1;
+            headsOfArrays.set(currMin.idx, nextMinIdx);
+            List<Integer> sortedArray = sortedArrays.get(currMin.idx);
+            if (headsOfArrays.get(currMin.idx) >= sortedArray.size()) {
+                return minDistance;
+            }
+            minElements.pollFirst();
+            minElements.add(new ArrayData(currMin.idx, sortedArray.get(nextMinIdx)));
+        }
     }
 
     public static void main(String[] args) {
