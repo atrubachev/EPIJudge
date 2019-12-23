301c146ba993e06857cc7c5c5636c88ef4c92569
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-12-23 22:21:47 +0100

    Add the solution of Generate All Non-Attacking Placements Of N-Queens problem (page 285 (java book))

--- epi_judge_java/epi/NQueens.java
+++ epi_judge_java/epi/NQueens.java
@@ -2,9 +2,10 @@ package epi;
 
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
-import epi.test_framework.LexicographicalListComparator;
 import epi.test_framework.GenericTest;
+import epi.test_framework.LexicographicalListComparator;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiPredicate;
 
@@ -12,20 +13,46 @@ public class NQueens {
     @EpiTest(testDataFile = "n_queens.tsv")
 
     public static List<List<Integer>> nQueens(int n) {
-        // TODO - you fill in here.
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        solveNQueens(n, 0, new ArrayList<>(), result);
+        return result;
     }
 
-    @EpiTestComparator
-    public static BiPredicate<List<List<Integer>>, List<List<Integer>>> comp =
-            (expected, result) -> {
-                if (result == null) {
-                    return false;
+    private static void solveNQueens(int n, int row, List<Integer> colPlacement, List<List<Integer>> result) {
+        if (row == n) {
+            result.add(new ArrayList<>(colPlacement));
+        } else {
+            for (int i = 0; i < n; i++) {
+                colPlacement.add(i);
+                if (isValid(colPlacement)) {
+                    solveNQueens(n, row + 1, colPlacement, result);
                 }
-                expected.sort(new LexicographicalListComparator<>());
-                result.sort(new LexicographicalListComparator<>());
-                return expected.equals(result);
-            };
+                colPlacement.remove(colPlacement.size() - 1);
+            }
+        }
+    }
+
+    private static boolean isValid(List<Integer> colPlacement) {
+        int currRow = colPlacement.size() - 1;
+        for (int row = 0; row < currRow; row++) {
+            int diff = Math.abs(colPlacement.get(row) - colPlacement.get(currRow));
+            if (diff == 0 || diff == currRow - row) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    @EpiTestComparator
+    public static BiPredicate<List<List<Integer>>, List<List<Integer>>> comp = (expected, result) -> {
+        if (result == null) {
+            return false;
+        }
+        expected.sort(new LexicographicalListComparator<>());
+        result.sort(new LexicographicalListComparator<>());
+        return expected.equals(result);
+    };
 
     public static void main(String[] args) {
         System.exit(
