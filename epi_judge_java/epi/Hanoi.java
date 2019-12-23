fc6d57ace9f416bab60d9635230213aa3f0d1e3b
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-12-23 21:22:57 +0100

    Add the solution of The Tower Of Hanoi problem (page 283 (java book))

--- epi_judge_java/epi/Hanoi.java
+++ epi_judge_java/epi/Hanoi.java
@@ -6,18 +6,36 @@ import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Deque;
-import java.util.List;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Hanoi {
 
     private static final int NUM_PEGS = 3;
+    private static List<List<Integer>> moves;
 
     public static List<List<Integer>> computeTowerHanoi(int numRings) {
-        // TODO - you fill in here.
-        return Collections.emptyList();
+        moves = new ArrayList<>();
+        List<Deque<Integer>> pegs = new ArrayList<>();
+        for (int i = 0; i < NUM_PEGS; i++) {
+            pegs.add(new LinkedList<>());
+        }
+        for (int i = numRings; i >= 1; i--) {
+            pegs.get(0).addFirst(i);
+        }
+        computeTowerHanoi(numRings, pegs, 0, 1, 2);
+        return moves;
+    }
+
+    public static void computeTowerHanoi(int numRings, List<Deque<Integer>> pegs, int from, int to, int use) {
+        if (numRings <= 0) {
+            return;
+        }
+        computeTowerHanoi(numRings - 1, pegs, from, use, to);
+        pegs.get(to).addFirst(pegs.get(from).removeFirst());
+        moves.add(List.of(from, to));
+        computeTowerHanoi(numRings - 1, pegs, use, to, from);
     }
 
     @EpiTest(testDataFile = "hanoi.tsv")
