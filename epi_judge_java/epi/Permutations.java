bc82b5d3538fe97af6bbc9b84267b053fbb07638
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-12-24 21:41:34 +0100

    Add the solution of Generate Permutations problem (page 287 (java book))

--- epi_judge_java/epi/Permutations.java
+++ epi_judge_java/epi/Permutations.java
@@ -2,10 +2,12 @@ package epi;
 
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
-import epi.test_framework.LexicographicalListComparator;
 import epi.test_framework.GenericTest;
+import epi.test_framework.LexicographicalListComparator;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.function.BiPredicate;
 
@@ -13,8 +15,26 @@ public class Permutations {
     @EpiTest(testDataFile = "permutations.tsv")
 
     public static List<List<Integer>> permutations(List<Integer> A) {
-        // TODO - you fill in here.
-        return null;
+        return computePermutations(A, 0);
+    }
+
+    private static List<List<Integer>> computePermutations(List<Integer> A, int currIdx) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (currIdx + 1 >= A.size()) {
+            List<Integer> perm = new LinkedList<>();
+            perm.add(A.get(currIdx));
+            result.add(perm);
+            return result;
+        }
+        for (int i = currIdx; i < A.size(); i++) {
+            Collections.swap(A, currIdx, i);
+            for (List<Integer> permutation : computePermutations(A, currIdx + 1)) {
+                permutation.add(0, A.get(currIdx));
+                result.add(permutation);
+            }
+            Collections.swap(A, currIdx, i);
+        }
+        return result;
     }
 
     @EpiTestComparator
