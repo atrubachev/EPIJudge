b6b8e9766ec2000b8d073e02914a358ba9f66f22
Andrey Trubachev <trubachev.andrey@gmail.com>
2019-12-26 09:51:28 +0100

    Add the solution of Generate All Subsets Of Size K problem (page 291 (java book))

--- epi_judge_java/epi/Combinations.java
+++ epi_judge_java/epi/Combinations.java
@@ -2,10 +2,10 @@ package epi;
 
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
-import epi.test_framework.LexicographicalListComparator;
 import epi.test_framework.GenericTest;
+import epi.test_framework.LexicographicalListComparator;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiPredicate;
 
@@ -13,8 +13,23 @@ public class Combinations {
     @EpiTest(testDataFile = "combinations.tsv")
 
     public static List<List<Integer>> combinations(int n, int k) {
-        // TODO - you fill in here.
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        directedCombinations(n, k, 1, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static void directedCombinations(int n, int k, int offset, ArrayList<Integer> partialCombination,
+                                             List<List<Integer>> result) {
+        if (k == partialCombination.size()) {
+            result.add(new ArrayList<>(partialCombination));
+            return;
+        }
+        int numRemaining = k - partialCombination.size();
+        for (int i = offset; i <= n && numRemaining <= n - 1 + 1; i++) {
+            partialCombination.add(i);
+            directedCombinations(n, k, i + 1, partialCombination, result);
+            partialCombination.remove(partialCombination.size() - 1);
+        }
     }
 
     @EpiTestComparator
