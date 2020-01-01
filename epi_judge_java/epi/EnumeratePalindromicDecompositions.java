84c3df7d0a24ea9854dfabfcee27f4c717e00f0b
Andrey Trubachev <trubachev.andrey@gmail.com>
2020-01-01 14:18:45 +0100

    Add the solution of Generate Palindromic Decompositions problem (page 293 (java book))

--- epi_judge_java/epi/EnumeratePalindromicDecompositions.java
+++ epi_judge_java/epi/EnumeratePalindromicDecompositions.java
@@ -2,18 +2,44 @@ package epi;
 
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
-import epi.test_framework.LexicographicalListComparator;
 import epi.test_framework.GenericTest;
+import epi.test_framework.LexicographicalListComparator;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiPredicate;
 
 public class EnumeratePalindromicDecompositions {
     @EpiTest(testDataFile = "enumerate_palindromic_decompositions.tsv")
 
-    public static List<List<String>> palindromeDecompositions(String input) {
-        // TODO - you fill in here.
-        return null;
+    public static List<List<String>> palindromeDecompositions(String s) {
+        List<List<String>> result = new ArrayList<>();
+        palindromeDecompositions(s, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static void palindromeDecompositions(String s, int offset, ArrayList<String> decomposition, List<List<String>> result) {
+        if (offset == s.length()) {
+            result.add(new ArrayList<>(decomposition));
+            return;
+        }
+        for (int i = offset + 1; i <= s.length(); i++) {
+            String elementOfDecomposition = s.substring(offset, i);
+            if (isPolindrom(elementOfDecomposition)) {
+                decomposition.add(elementOfDecomposition);
+                palindromeDecompositions(s, i, decomposition, result);
+                decomposition.remove(decomposition.size() - 1);
+            }
+        }
+    }
+
+    private static boolean isPolindrom(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @EpiTestComparator
