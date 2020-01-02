248fcd99335d5f74ea86f47cde8264f25483270f
Andrey Trubachev <trubachev.andrey@gmail.com>
2020-01-02 21:16:00 +0100

    Add the solution of Count The Number Of Score Combinations problem (page 306 (java book))

--- epi_judge_java/epi/NumberOfScoreCombinations.java
+++ epi_judge_java/epi/NumberOfScoreCombinations.java
@@ -8,11 +8,23 @@ import java.util.List;
 public class NumberOfScoreCombinations {
     @EpiTest(testDataFile = "number_of_score_combinations.tsv")
 
-    public static int
-    numCombinationsForFinalScore(int finalScore,
-                                 List<Integer> individualPlayScores) {
-        // TODO - you fill in here.
-        return 0;
+    public static int numCombinationsForFinalScore(int finalScore, List<Integer> individualPlayScores) {
+        int[][] numCombinationsForScore = new int[individualPlayScores.size()][finalScore + 1];
+        for (int combination = 0; combination < individualPlayScores.size(); combination++) {
+            numCombinationsForScore[combination][0] = 1;
+            for (int score = 1; score <= finalScore; score++) {
+                int withoutPlay = 0;
+                if (combination - 1 >= 0) {
+                    withoutPlay = numCombinationsForScore[combination - 1][score];
+                }
+                int withPlay = 0;
+                if (score >= individualPlayScores.get(combination)) {
+                    withPlay = numCombinationsForScore[combination][score - individualPlayScores.get(combination)];
+                }
+                numCombinationsForScore[combination][score] = withoutPlay + withPlay;
+            }
+        }
+        return numCombinationsForScore[individualPlayScores.size() - 1][finalScore];
     }
 
     public static void main(String[] args) {
