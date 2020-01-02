7e83269cd733342e75925262815a34e09b842f8c
Andrey Trubachev <trubachev.andrey@gmail.com>
2020-01-02 21:25:15 +0100

    Add the solution of Count The Number Of Ways To Traverse A 2D Array problem (page 312 (java book))

--- epi_judge_java/epi/NumberOfTraversalsMatrix.java
+++ epi_judge_java/epi/NumberOfTraversalsMatrix.java
@@ -7,8 +7,19 @@ public class NumberOfTraversalsMatrix {
     @EpiTest(testDataFile = "number_of_traversals_matrix.tsv")
 
     public static int numberOfWays(int n, int m) {
-        // TODO - you fill in here.
-        return 0;
+        int[][] map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            map[i][0] = 1;
+        }
+        for (int j = 0; j < m; j++) {
+            map[0][j] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                map[i][j] = map[i - 1][j] + map[i][j - 1];
+            }
+        }
+        return map[n - 1][m - 1];
     }
 
     public static void main(String[] args) {
