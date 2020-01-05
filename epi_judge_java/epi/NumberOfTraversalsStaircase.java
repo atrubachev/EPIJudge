dfe4e14101b5720eb5d723681b03992b00f8de30
Andrey Trubachev <trubachev.andrey@gmail.com>
2020-01-05 19:13:00 +0100

    Add the solution of Count The Number Of Moves To Climb Stairs problem (page 326 (java book))

--- epi_judge_java/epi/NumberOfTraversalsStaircase.java
+++ epi_judge_java/epi/NumberOfTraversalsStaircase.java
@@ -7,8 +7,21 @@ public class NumberOfTraversalsStaircase {
     @EpiTest(testDataFile = "number_of_traversals_staircase.tsv")
 
     public static int numberOfWaysToTop(int top, int maximumStep) {
-        // TODO - you fill in here.
-        return 0;
+        if (top < 2) {
+            return 1;
+        }
+        int[] cache = new int[top + 1];
+        cache[0] = 1;
+        cache[1] = 1;
+        for (int i = 2; i <= top; i++) {
+            for (int j = 1; j <= maximumStep; j++) {
+                if (i - j < 0) {
+                    break;
+                }
+                cache[i] += cache[i - j];
+            }
+        }
+        return cache[top];
     }
 
     public static void main(String[] args) {
