d264b084c71023710f920c7d0a807b59f2a8142f
Andrey Trubachev <trubachev.andrey@gmail.com>
2020-01-06 07:07:32 +0100

    Add the solution of Find The Longest Non-Decreasing Subsequence problem (page 330 (java book))

--- epi_judge_java/epi/LongestNondecreasingSubsequence.java
+++ epi_judge_java/epi/LongestNondecreasingSubsequence.java
@@ -3,14 +3,26 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LongestNondecreasingSubsequence {
     @EpiTest(testDataFile = "longest_nondecreasing_subsequence.tsv")
 
     public static int longestNondecreasingSubsequenceLength(List<Integer> A) {
-        // TODO - you fill in here.
-        return 0;
+        List<Integer> longestByEnd = new ArrayList<>(A.size());
+        for (int i = 0; i < A.size(); i++) {
+            longestByEnd.add(1);
+        }
+        for (int i = 1; i < A.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (A.get(i).compareTo(A.get(j)) >= 0) {
+                    longestByEnd.set(i, Math.max(longestByEnd.get(i), longestByEnd.get(j) + 1));
+                }
+            }
+        }
+        return Collections.max(longestByEnd);
     }
 
     public static void main(String[] args) {
