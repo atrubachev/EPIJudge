68109bc75db1b5dd26c870e3aa6dfd72dc624ab2
Andrey Trubachev <trubachev.andrey@gmail.com>
2020-01-08 06:21:06 +0100

    Add the solution of The 3-Sum Problem (page 340 (java book))

--- epi_judge_java/epi/ThreeSum.java
+++ epi_judge_java/epi/ThreeSum.java
@@ -3,14 +3,22 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ThreeSum {
     @EpiTest(testDataFile = "three_sum.tsv")
 
     public static boolean hasThreeSum(List<Integer> A, int t) {
-        // TODO - you fill in here.
-        return true;
+        Collections.sort(A);
+        for (Integer num1 : A) {
+            for (Integer num2 : A) {
+                if (Collections.binarySearch(A, t - num1 - num2) >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
