fe6301f3b9cac225f00ed4d2e08cd23d324df25d
Andrey Trubachev <trubachev.andrey@gmail.com>
2020-02-01 10:35:50 +0100

    Add the solution of Interconvert Strings And Integers problem (page 14, python book)

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -1,15 +1,38 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+CH_TO_DIGIT = {str(i): i for i in range(10)}
+DIGIT_TO_CH = {i: str(i) for i in range(10)}
 
-def int_to_string(x):
-    # TODO - you fill in here.
-    return ''
+def int_to_string(x) -> str:
+    chars = list()
+    sign = 1
+
+    if x == 0:
+        chars.append(DIGIT_TO_CH[0])
+    if x < 0:
+        sign = -1
+        x *= -1
+
+    while x != 0:
+        chars.append(DIGIT_TO_CH[x % 10])
+        x //= 10
+
+    if sign == -1:
+        chars.append('-')
+    return ''.join(reversed(chars))
 
 
-def string_to_int(s):
-    # TODO - you fill in here.
-    return 0
+def string_to_int(s) -> int:
+    num = 0
+    sign = 1
+    for ch in s:
+        if ch == '-':
+            sign = -1
+        else:
+            num = num * 10 + CH_TO_DIGIT[ch]
+
+    return num * sign
 
 
 def wrapper(x, s):
